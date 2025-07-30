2fd4024a3a00121518d71875d5ada506a681aef4
Ravi Tharun <tr565003@gmail.com>
2025-07-30 22:16:12 +0530

    Added the .toml file

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,20 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-// 
-// // https://vite.dev/config/
-// export default defineConfig({
-  //   plugins: [tailwindcss (),react()],
-// })
 import { defineConfig } from 'vite'
-  import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
-  build: {
-    rollupOptions: {
-      external: ['react-is']
-    }
-  }
+    plugins: [tailwindcss (),react()],
 })
+
